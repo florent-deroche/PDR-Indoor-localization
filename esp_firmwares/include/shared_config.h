2212583ef645b816e1fa41cf6166af8489c312eb
lois-goon <loisgonce.pro@gmail.com>
2026-02-27 21:16:12 +0100

    main_robot.cpp programming, grabbing Anchors bluetooth RSSI and sending data via json packets to the mosquitto MQTT serv, fully working with phone wifi hotsot

--- esp_firmwares/include/shared_config.h
+++ esp_firmwares/include/shared_config.h
@@ -1,3 +1,18 @@
+#ifndef SHARED_CONFIG_H
+#define SHARED_CONFIG_H
+
+
 // we define here all the shared config off the esp32 like Names of the diff anchor etc... to make ble transmitting easier 
+#define ANCHOR_1_NAME "ESP32_Anchor_ID:1"
+#define ANCHOR_2_NAME "ESP32_Anchor_ID:2"  
+#define ANCHOR_3_NAME "ESP32_Anchor_ID:3"
 
+// wifi and mqtt config
+#define ssid "Netsphere_Terminal"
+#define password "goon1234"
 
+// MQTT config
+#define mqtt_port 1883
+#define mqtt_server "10.36.126.210"
+
+#endif  
--- esp_firmwares/src/robot/main_robot.cpp
+++ esp_firmwares/src/robot/main_robot.cpp
@@ -2,5 +2,134 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include <BLEScan.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h> 
+#include <BLEAdvertisedDevice.h>
+
+// We want fast publication of result, python ros node will filter abberant value for RSSI. 
+
+// Wifi ID, MQTT, anchor Names Id
 #include "shared_config.h"
 
+WiFiClient espClient; 
+PubSubClient mqttClient(espClient);
+BLEScan* pBLEScan; 
+
+// init rssi signal value to very very low (-100dBm)
+int rssi_anchor_1 = -100;
+int rssi_anchor_2 = -100;
+int rssi_anchor_3 = -100; 
+
+const int SCAN_TIME = 500; // in milliseconds
+
+// automatically called function when a BLE device is detected (Callback function)
+
+class DetectedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice detectedDevice) {
+        if(detectedDevice.haveName()) {
+            String deviceName = detectedDevice.getName().c_str();
+            int rssi = detectedDevice.getRSSI();
+
+            // we keep only the signals from our anchors, we filter other devices
+            if (deviceName == ANCHOR_1_NAME) rssi_anchor_1 = rssi; // from shared_config.h
+            else if (deviceName == ANCHOR_2_NAME) rssi_anchor_2 = rssi; 
+            else if (deviceName == ANCHOR_3_NAME) rssi_anchor_3 = rssi;
+
+        };
+
+    };
+
+
+};
+
+void setup_wifi() {
+    delay(10);
+    Serial.println("Connecting to WiFi:");
+    Serial.println(ssid); // defined in shared_config.h
+    WiFi.begin(ssid, password);
+
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print("."); 
+    }
+    Serial.println("\nSuccesfully connected to WiFi ! IP : ");
+    Serial.println(WiFi.localIP());
+}
+
+void reconnect_mqtt() {
+    while(!mqttClient.connected()) {
+        Serial.print("Connecting to MQTT Serv...");
+        String clientId = "RobotESP32-1"; // change this if we want to localise various robots
+        if (mqttClient.connect(clientId.c_str())) {
+            Serial.println("Connected to mqtt broker ! ");
+
+        } else {
+            Serial.print("Connection failure, error : ");
+            Serial.print(mqttClient.state());
+            Serial.print("Retrying in 5 seconds");
+            delay(5000);
+        }
+
+    }
+
+
+}
+
+void setup() {
+    Serial.begin(115200);
+
+    // wifi mqtt serv launch 
+    setup_wifi(); 
+    mqttClient.setServer(mqtt_server, mqtt_port); // see shared_config.h
+
+    // BLE scan launch 
+    Serial.println("Initializing BLE Scanner...");
+    BLEDevice::init("");
+    pBLEScan = BLEDevice::getScan();
+    pBLEScan->setAdvertisedDeviceCallbacks(new DetectedDeviceCallbacks);
+    pBLEScan->setActiveScan(true);
+    pBLEScan->setInterval(100); 
+    pBLEScan->setWindow(99); 
+
+
+}
+
+void loop() {
+
+    // mqtt connection verification 
+    if (!mqttClient.connected()) {
+        reconnect_mqtt();
+    }
+    mqttClient.loop();
+
+
+    // BLE scan launching at 500 ms
+    Serial.printf("Scanning BLE (%dms)...", SCAN_TIME);
+    pBLEScan->start(0,nullptr,false);
+    delay(SCAN_TIME); 
+    pBLEScan->stop(); 
+    pBLEScan->clearResults(); //clearing memory
+
+    // creating JSON packet for RSSI data from the anchors
+    StaticJsonDocument<200> document; 
+    document["Anchor_1"] = rssi_anchor_1;
+    document["Anchor_2"] = rssi_anchor_2;
+    document["Anchor_3"] = rssi_anchor_3; 
+
+    char jsonBuffer[512]; 
+    serializeJson(document, jsonBuffer); 
+
+    // sending to mqtt server 
+
+    Serial.print("Publication MQTT: "); 
+    Serial.println(jsonBuffer);
+    mqttClient.publish("PDR/robot/rssi",jsonBuffer);
+
+    //resetting anchor rssi to very very far 
+    rssi_anchor_1 = -100;
+    rssi_anchor_2 = -100;
+    rssi_anchor_3 = -100; 
+
+}
