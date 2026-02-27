2aa371f4719fb32617540be982f463892ae6762d
lois-goon <loisgonce.pro@gmail.com>
2026-02-27 18:23:10 +0100

    Add of MQQT server folder with config, implementation of anchor cpp code, shared_config.h creation, robot_main.cpp creation

---
+++ esp_firmwares/include/shared_config.h
@@ -0,0 +1,3 @@
+// we define here all the shared config off the esp32 like Names of the diff anchor etc... to make ble transmitting easier 
+
+
---
+++ esp_firmwares/src/robot/main_robot.cpp
@@ -0,0 +1,6 @@
+#include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include "shared_config.h"
+
