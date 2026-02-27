c4f86eb49b899d8a23bf0d35c3a1915ebbfb6232
lois-goon <loisgonce.pro@gmail.com>
2026-02-27 21:32:11 +0100

    Ros package update, now in python with Apache license

---
+++ ros2_ws/localisation_pkg/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'localisation_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='loisgoon',
+    maintainer_email='loisgonce.pro@gmail.com',
+    description='Indoor localisation with BLE trilateration without GNSS signals ',
+    license='Apache License 2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
