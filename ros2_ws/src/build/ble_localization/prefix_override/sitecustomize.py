926dbe8895a09726f201d2a69517704390544750
lois-goon <loisgonce.pro@gmail.com>
2026-03-13 15:21:34 +0100

    Cleaning accidental push of build files

--- ros2_ws/src/build/ble_localization/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/mnt/c/Users/lois5/Documents/PC LOIS GOON/projet/PDR/PDR-Indoor-localization/ros2_ws/src/install/ble_localization'
