Real condition experience with our program, problems with offset of tf frames (odom, ble_origin, map), fixed abberant bias for the mean and variance correction, fully working architecture

ros2_ws/src/ble_localization/ble_localization/trilateration_node.py
@@ -1,28 +1,27 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Point
+from geometry_msgs.msg import PoseWithCovarianceStamped
 import paho.mqtt.client as mqtt 
 import json
 import math
 from scipy.optimize import least_squares
 
 # --- CONFIG ---
-IP_ADRESS   = '10.120.2.235' #mqtt address
+IP_ADRESS   = '10.120.2.231' 
 PORT        = 1883
 TOPIC       = 'PDR/robot/rssi'
 
 # Anchor coordinates (X,Y) in meters
 ANCHORS_POS = {
-    'Anchor_1': (2.15, 0.0),
-    'Anchor_2': (1.05, 1.3),
-    'Anchor_3': (0.0, 0.0)
+    'Anchor_1': (2.36, 5.92),
+    'Anchor_2': (9.94, 0.25),
+    'Anchor_3': (0.6, 1.5)
 }
 
 # Signal propagation model constants 
-A_CONST = -33.0 
+A_CONST = -40 
 N_CONST = 3.8   
 
-# 1D Kalman filter to smooth RSSI data 
 class KalmanFilter1D:
     def __init__(self, q, r, p, initial_value):
         self.q = q
@@ -38,23 +37,21 @@ class KalmanFilter1D:
             self.p = (1 - k) * self.p
         return self.x
 
-# ROS2 Node for trilateration based on MQTT RSSI data
 class TrilaterationNode(Node):
     def __init__(self):
         super().__init__('ble_trilateration_node')
         
-        # Publisher node creation for estimated position
-        self.position_publisher = self.create_publisher(Point, 'ble_estimated_position', 10)
+        self.position_publisher = self.create_publisher(PoseWithCovarianceStamped, 'ble_estimated_position', 10)
         
-        # Filter init for each anchor
         self.filtres_ancres = {
             'Anchor_1': KalmanFilter1D(q=0.05, r=25.0, p=1.0, initial_value=-60.0),
             'Anchor_2': KalmanFilter1D(q=0.05, r=25.0, p=1.0, initial_value=-60.0),
             'Anchor_3': KalmanFilter1D(q=0.05, r=25.0, p=1.0, initial_value=-60.0)
         }
 
-        # MQTT Config 
+       
         self.mqtt_client = mqtt.Client()
+        
         self.mqtt_client.on_connect = self.on_mqtt_connect
         self.mqtt_client.on_message = self.on_mqtt_message
         
@@ -62,7 +59,7 @@ class TrilaterationNode(Node):
         self.mqtt_client.connect(IP_ADRESS, PORT, 60)
         self.mqtt_client.loop_start() 
 
-    def on_mqtt_connect(self, client, userdata, flags, reason_code, properties):
+    def on_mqtt_connect(self, client, userdata, flags, reason_code, properties=None):
         if reason_code == 0:
             self.get_logger().info("Connection succesful, waiting for RSSI data...")
             client.subscribe(TOPIC)
@@ -70,38 +67,38 @@ class TrilaterationNode(Node):
             self.get_logger().error(f"Connection failure : {reason_code}")
 
     def on_mqtt_message(self, client, userdata, msg):
+        
+        self.get_logger().info(f"Message brut reçu : {msg.payload.decode('utf-8')}")
+        
         try:
-            # JSON parsing of the incoming message
             donnees = json.loads(msg.payload.decode('utf-8'))
             distances = {}
             
-            # Processing each anchor's RSSI with Kalman filter and converting to distance
             for ancre, rssi in donnees.items():
                 mesure_valide = (rssi != -100)
                 rssi_filtre = self.filtres_ancres[ancre].process(measurement=rssi, is_valid=mesure_valide)
-                
-                
                 distances[ancre] = 10 ** ((A_CONST - rssi_filtre) / (10 * N_CONST))
 
-            
             x, y = self.calculate_position(distances)
             
-            # Publishing on ROS Topic 
-            position_msg = Point()
-            position_msg.x = x
-            position_msg.y = y
-            position_msg.z = 0.0 
+            pose_msg = PoseWithCovarianceStamped()
+            pose_msg.header.stamp = self.get_clock().now().to_msg()
+            pose_msg.header.frame_id = 'ble_origin' 
             
-            self.position_publisher.publish(position_msg)
-            self.get_logger().debug(f"Published position : X={x:.2f}, Y={y:.2f}")
+            pose_msg.pose.pose.position.x = x
+            pose_msg.pose.pose.position.y = y
+            pose_msg.pose.pose.position.z = 0.0 
+            pose_msg.pose.pose.orientation.w = 1.0
+            
+            pose_msg.pose.covariance[0] = 0.5
+            pose_msg.pose.covariance[7] = 0.5
+            
+            self.position_publisher.publish(pose_msg)
 
         except Exception as e:
             self.get_logger().error(f"Error processing MQTT message: {e}")
 
-
-    # TO DO : Change this or remove it if you don't need it, it's just an example of a method that could be used in the error function for least squares optimization
     def error_function(self, guess, anchors, distances):
-        """Least squares error function for trilateration"""
         errors = []
         for (ax, ay), d in zip(anchors, distances):
             calc_dist = math.sqrt((guess[0] - ax)**2 + (guess[1] - ay)**2)
@@ -109,7 +106,6 @@ class TrilaterationNode(Node):
         return errors
 
     def calculate_position(self, distances_dict):
-        """Calculate the position (X,Y) by minimizing the error on the 3 distances"""
         anchors_list = []
         distances_list = []
         
@@ -124,11 +120,9 @@ class TrilaterationNode(Node):
         x_brut = result.x[0]
         y_brut = result.x[1]
         
-        # Bias correction based on empirical calibration
-        x_corrige = x_brut - 0.882
-        y_corrige = y_brut - 0.124
         
-        return x_corrige, y_corrige
+        
+        return float(x_brut, float(y_brut))
 
 def main():
     rclpy.init()

ros2_ws/src/sensor_fusion/sensor_fusion/fusion_node.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from geometry_msgs.msg import Point, PoseStamped
+from geometry_msgs.msg import PoseStamped, PoseWithCovarianceStamped
 from tf2_ros import TransformException, Buffer, TransformListener
 import numpy as np
 
@@ -8,119 +8,87 @@ class KalmanFusionNode(Node):
     def __init__(self):
         super().__init__('kalman_fusion_node')
 
-        # /--- Subscriptions and publications ---/
-        # Listen to the position calculated by the 'ble_localization' package
         self.subscription = self.create_subscription(
-            Point, 
+            PoseWithCovarianceStamped, 
             'ble_estimated_position', 
             self.ble_callback, 
             10
         )
-        # Publish the filtered pose for RViz2
         self.publisher = self.create_publisher(PoseStamped, 'fused_pose', 10)
 
-        # /--- Odometry management (TF) ---/
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
+        
+      
+
         self.last_odom_x = None
         self.last_odom_y = None
 
-        # /--- Kalman Filter Configuration ---/
-        # Intial state: X = [x, y]^T
         self.X = np.zeros((2, 1))
-        
-        # Uncertainty Matrix (P): Average confidence 
         self.P = np.eye(2) * 1.0 
-        
-        # Model noise (Q): Confidence in odometry (very reliable in the short term)
         self.Q = np.eye(2) * 0.05 
-        
-        # Noise of measurement (R): Confidence in BLE.
-        # Based on your real tests: error of ~1.3m -> Variance = 1.3^2 = 1.69
         self.R = np.eye(2) * 1.69 
 
-        # --- Prediction loop (10 Hz) ---
         self.timer = self.create_timer(0.1, self.prediction_step)
         self.get_logger().info("Kalman Fusion Node Started, Waiting for odometry...")
 
     def prediction_step(self):
-        """ Prediction step based on Odometry """
         try:
-            # Get the real displacement of the robot via odometry
             now = rclpy.time.Time()
+            # On écoute juste de combien le robot a bougé depuis la dernière fois
             trans = self.tf_buffer.lookup_transform('odom', 'base_footprint', now)
             
-            curr_x = trans.transform.translation.x
-            curr_y = trans.transform.translation.y
+            curr_odom_x = trans.transform.translation.x
+            curr_odom_y = trans.transform.translation.y
 
-            # Initialization at the first pass
             if self.last_odom_x is None:
-                self.last_odom_x = curr_x
-                self.last_odom_y = curr_y
-                self.X[0, 0] = curr_x
-                self.X[1, 0] = curr_y
+                self.last_odom_x = curr_odom_x
+                self.last_odom_y = curr_odom_y
                 return
 
-            # Calculate the displacement vector (Delta odom)
-            dx = curr_x - self.last_odom_x
-            dy = curr_y - self.last_odom_y
+            dx = curr_odom_x - self.last_odom_x
+            dy = curr_odom_y - self.last_odom_y
 
-            # --- Kalman prediciton ---
             self.X[0, 0] += dx
             self.X[1, 0] += dy
             
-            # P = P + Q 
             self.P = self.P + self.Q
 
-        
-            self.last_odom_x = curr_x
-            self.last_odom_y = curr_y
+            self.last_odom_x = curr_odom_x
+            self.last_odom_y = curr_odom_y
 
-    
             self.publish_fused_pose()
 
         except TransformException:
-    
             pass
 
     def ble_callback(self, msg):
-        """ Update based on BLE measurement """
-        if self.last_odom_x is None:
+        ble_x = msg.pose.pose.position.x
+        ble_y = msg.pose.pose.position.y
+
+        if self.last_odom_x is None or (self.X[0, 0] == 0 and self.X[1, 0] == 0):
+            self.X[0, 0] = ble_x
+            self.X[1, 0] = ble_y
             return 
 
-        # Measurement vector Z
-        Z = np.array([[msg.x], [msg.y]])
-        
-        
-        # Y = Z - X (Innovation)
+        Z = np.array([[ble_x], [ble_y]])
         Y = Z - self.X
-        
-        # Covariance of the Innovation (S = P + R)
         S = self.P + self.R
-        
-        # Kalman Gain (K = P * S^-1)
         K = np.dot(self.P, np.linalg.inv(S))
-        
-        # State update (X = X + K * Y)
         self.X = self.X + np.dot(K, Y)
         
-        # Covariance update (P = (I - K) * P)
         I = np.eye(2)
         self.P = np.dot((I - K), self.P)
 
-        self.get_logger().info(f'BLE correction applied : {np.trace(self.P):.2f}')
-
     def publish_fused_pose(self):
-        """ Edit and publish data for ROS2 Ecosystem. """
         msg = PoseStamped()
         msg.header.stamp = self.get_clock().now().to_msg()
-        msg.header.frame_id = 'odom' # Global reference frame
+        # On publie la position dans notre propre repère d'ancres !
+        msg.header.frame_id = 'ble_origin' 
         
         msg.pose.position.x = float(self.X[0, 0])
         msg.pose.position.y = float(self.X[1, 0])
         msg.pose.position.z = 0.0
-        
-        
         msg.pose.orientation.w = 1.0 
         
         self.publisher.publish(msg)

script_test_python/quick_experience_stat.py
@@ -6,7 +6,7 @@ import time
 from scipy.optimize import least_squares
 
 # --- CONFIGURATION ---
-IP_ADRESS   = '10.120.2.235'
+IP_ADRESS   = '10.120.2.231'
 PORT        = 1883
 TOPIC       = "PDR/robot/rssi"
 DUREE_TEST  = 30 # Secondes d'enregistrement